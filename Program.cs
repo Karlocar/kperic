543925eb9515ab1f746a39d6e3d296665311462a
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-07-05 22:13:42 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,56 +1,13 @@
-﻿int redaka = 5, stupaca = 5;
-Console.Write("Unesi broj redova:   ");
-redaka = int.Parse(Console.ReadLine());
-Console.Write("Unesi broj stupaca: ");
-stupaca = int.Parse(Console.ReadLine());
-int[,] matrica = new int[redaka, stupaca];
+﻿using System.Reflection.Metadata;
 
-for (int i = 0; i < redaka; i++)
+int Tip3()
 {
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + " ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("***********************");
-
-int b = 1;
-
-//matrica [redaka - 1, stupaca - 1] = b++;
-//matrica [redaka - 1, stupaca  - 3] = b++;
-//matrica [redaka - 1, stupaca  - 5] = b++;
-//matrica [redaka - 1, stupaca  - 2] = b++;
-//matrica [redaka - 1, stupaca  - 4] = b++;
-//matrica [redaka - 1, stupaca  - 6] = b++;
-
-for (int i = 1; i <= stupaca; i++)
-{
-    matrica[redaka - 1, stupaca - i] = b++;
+    return new Random().Next();
 }
 
-for (int i = 0; i < redaka; i++)
-{
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + " ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("***********************");
-
-for (int i = redaka - 2; i >= 0; i--)
-{
-    matrica[i, 0] = b++;
-}
+Console.WriteLine(Tip3());
+int slucajniBroj = Tip3 ();
+Console.WriteLine(slucajniBroj);
 
 
-for (int i = 0; i < redaka; i++)
-{
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + " ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("***********************");
+
