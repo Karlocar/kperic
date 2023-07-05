1f4f5f4d68fe9396f91c65d05eeecccfd87354ae
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-07-05 22:34:57 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,3 @@
-﻿using System.Reflection.Metadata;
-
-int Tip3()
-{
-    return new Random().Next();
-}
-
-Console.WriteLine(Tip3());
-int slucajniBroj = Tip3 ();
-Console.WriteLine(slucajniBroj);
 
 
 
