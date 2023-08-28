19cc08b550c872d9683fe70b61589700e49a7711
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-08-28 12:17:47 +0200

    Add files via upload

---
+++ Osoba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ljetni_Rad
+{
+    internal class Osoba:Entitet
+    {
+        public String Ime { get; set; }
+        public String   Prezime { get; set; }
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+    }
+}
