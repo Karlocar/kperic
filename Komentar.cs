a2759f9e465952ef02d807b4b67c6e0877d36168
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-08-28 12:15:46 +0200

    Add files via upload

---
+++ Komentar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ljetni_Rad
+{
+    internal class Komentar:Entitet
+    
+    {
+        
+        
+        
+        public int Objava { get; set; }
+        public string Osoba { get; set; }
+    }
+}
