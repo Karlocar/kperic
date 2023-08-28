02ca34089bd27c15480f7b1fb43c5eb53b086ba2
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-08-28 12:16:57 +0200

    Add files via upload

---
+++ Objava.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ljetni_Rad
+{
+    internal class Objava : Entitet
+    {
+        
+        
+        public string Opis { get; set; }
+        public string IpAdresa { get; set; }
+        public string Osoba { get; set; }
+
+        public string Slika { get; set; }
+
+        public override string ToString()
+        {
+            return  Osoba + "" + IpAdresa;
+        }
+    }
+        
+    }
+
