3fa37b70e875e4a9f712228ca9adf1777344b94b
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-08-28 12:14:29 +0200

    Add files via upload

---
+++ Izbornik.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ljetni_Rad
+{
+    internal class Izbornik
+    {
+        private ObradaKomentar ObradaKomentar;
+        private ObradaObjava ObradaObjava;
+        private ObradaOsoba ObradaOsoba;
+        public Izbornik() 
+        {
+            ObradaKomentar = new ObradaKomentar();
+            ObradaObjava = new ObradaObjava();
+            ObradaOsoba = new ObradaOsoba();
+            PozdravnaPoruka();
+            PrikaziIzbornik();
+        } 
+        private void PozdravnaPoruka()
+        
+       {
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("****** Instagram Console APP v 1.0 ******");
+            Console.WriteLine("*****************************************");
+            
+
+        }
+        public void PrikaziIzbornik()
+        {
+            Console.WriteLine("Glavni izbornik");
+            Console.WriteLine("1. Komentar");
+            
+            Console.WriteLine("2. Objava");
+            Console.WriteLine("3. Osoba");
+            Console.WriteLine("4. Izlaz iz programa ");
+            
+            switch (Pomocno.ucitajBrojRaspon("Odaberite stavku izbornika: ",
+                "Odabir mora biti 1 - 4.",1,4))
+            {
+                case 1:
+                    ObradaKomentar.PrikaziIzbornik();
+                    PrikaziIzbornik();
+                    break;
+                                
+                    case 2:
+                    ObradaObjava.PrikaziIzbornik();
+                    PrikaziIzbornik();
+                    break;
+                    case 3:
+                    ObradaOsoba.PrikaziIzbornik();
+                    PrikaziIzbornik();
+                    break;
+                    case 4:
+                    Console.WriteLine("Hvala na korištenju, doviđenja");
+                    break;
+            }
+        }
+
+
+
+
+    }
+}
