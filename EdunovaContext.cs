fa57953da50160648b3679860e891d1304239120
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-09-11 13:45:03 +0200

    Add files via upload

---
+++ EdunovaContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Instagram.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Instagram.Data
+{
+    public class EdunovaContext : DbContext
+    {
+        public EdunovaContext(DbContextOptions<EdunovaContext> opcije)
+        : base(opcije) { 
+    }
+        public DbSet<Osoba> Osoba { get; set; }
+    }
+}
